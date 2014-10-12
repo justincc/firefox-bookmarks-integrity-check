cac4989d00cb789113ab97d20d2ae40483921e0d
Justin Clark-Casey <justincc@justincc.org>
2014-10-12 23:41:26 +0100

    refactor: move checking code into a check() function

--- src/fbic.py
+++ src/fbic.py
@@ -3,6 +3,33 @@
 import argparse
 import sqlite3
 
+def check(dbPath):
+    conn = sqlite3.connect(dbPath)
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor()
+    
+    curs.execute("select id from moz_bookmarks")
+    
+    # Record the built-in root folder with ID 0
+    ids = [ 0 ]
+    
+    for row in curs:
+        ids.append(row['id'])
+    
+    curs.execute("select id, parent, title from moz_bookmarks")
+    
+    entriesWithoutValidParent = []
+    
+    for row in curs:
+        if row['parent'] not in ids:
+            entriesWithoutValidParent.append(row)
+            
+    conn.close()    
+    
+    print "Database %s contains:" % dbPath        
+    print "Entries: %s" % (len(ids) - 1)
+    print "Entries without valid parent: %s" % len(entriesWithoutValidParent)
+    
 ############
 ### MAIN ###
 ############
@@ -12,28 +39,4 @@ parser.add_argument('dbPath', help = 'Path to Firefox places.sqlite file.', meta
 
 opt = parser.parse_args()
 
-conn = sqlite3.connect(opt.dbPath)
-conn.row_factory = sqlite3.Row
-curs = conn.cursor()
-
-curs.execute("select id from moz_bookmarks")
-
-# Record the built-in root folder with ID 0
-ids = [ 0 ]
-
-for row in curs:
-    ids.append(row['id'])
-
-curs.execute("select id, parent, title from moz_bookmarks")
-
-entriesWithoutValidParent = []
-
-for row in curs:
-    if row['parent'] not in ids:
-        entriesWithoutValidParent.append(row)
-        
-conn.close()    
-
-print "Database %s contains:" % opt.dbPath        
-print "Entries: %s" % (len(ids) - 1)
-print "Entries without valid parent: %s" % len(entriesWithoutValidParent)
+check(opt.dbPath)
