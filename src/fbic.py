174655d6140998a5e661cd5ec8de7a5b6ea0a634
Justin Clark-Casey <justincc@justincc.org>
2014-10-12 23:58:07 +0100

    refactor: move check code into a separate fbic package

--- src/fbic.py
+++ src/fbic.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python
 
 import argparse
-import sqlite3
-
-def check(dbPath):
-    conn = sqlite3.connect(dbPath)
-    conn.row_factory = sqlite3.Row
-    curs = conn.cursor()
+import fbic.checker
     
-    curs.execute("select id from moz_bookmarks")
-    
-    # Record the built-in root folder with ID 0
-    ids = [ 0 ]
-    
-    for row in curs:
-        ids.append(row['id'])
-    
-    curs.execute("select id, parent, title from moz_bookmarks")
-    
-    invalidParentEntries = []
-    
-    for row in curs:
-        if row['parent'] not in ids:
-            invalidParentEntries.append(row)
-            
-    conn.close()
-    
-    return { 'entries' : len(ids) - 1, 'invalidParentEntries' : len(invalidParentEntries) }
+def yeck():
+    print "YEK"
     
 ############
 ### MAIN ###
@@ -37,7 +15,7 @@ parser.add_argument('dbPath', help = 'Path to Firefox places.sqlite file.', meta
 
 opt = parser.parse_args()
 
-results = check(opt.dbPath)
+results = fbic.checker.check(opt.dbPath)
     
 print "Database %s contains:" % opt.dbPath        
 print "Entries: %s" % results['entries']
---
+++ src/fbic/checker.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def check(dbPath):
+    conn = sqlite3.connect(dbPath)
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor()
+    
+    curs.execute("select id from moz_bookmarks")
+    
+    # Record the built-in root folder with ID 0
+    ids = [ 0 ]
+    
+    for row in curs:
+        ids.append(row['id'])
+    
+    curs.execute("select id, parent, title from moz_bookmarks")
+    
+    invalidParentEntries = []
+    
+    for row in curs:
+        if row['parent'] not in ids:
+            invalidParentEntries.append(row)
+            
+    conn.close()
+    
+    return { 'entries' : len(ids) - 1, 'invalidParentEntries' : len(invalidParentEntries) }
